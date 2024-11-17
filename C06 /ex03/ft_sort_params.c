275bd23c5b5e49782f37ff487487bb7a8bfd0f8d
cankyran <ptcankiran@gmail.com>
2024-11-17 21:51:47 +0300

    Add files via upload

---
+++ C06 /ex03/ft_sort_params.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mucankir <mucankir@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 21:43:38 by mucankir          #+#    #+#             */
+/*   Updated: 2024/09/05 00:13:53 by mucankir         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i++]);
+	}
+}
+
+int	ft_strcmp( char *str1, char *str2)
+{
+	int	i;
+
+	i = 0;
+	while (str1[i] || str2[i])
+	{
+		if (str1[i] == str2[i])
+			i++;
+		else
+			return (str1[i] - str2[i]);
+	}
+	return (0);
+}
+
+void	ft_swap(char **str1, char **str2)
+{
+	char	*tmp;
+
+	tmp = *str1;
+	*str1 = *str2;
+	*str2 = tmp;
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < ac - 1)
+	{
+		j = 1;
+		while (j < ac - 1)
+		{
+			if (ft_strcmp(av[j], av[j + 1]) > 0)
+			{
+				ft_swap(&av[j], &av[j + 1]);
+			}
+			j++;
+		}
+		i++;
+	}
+	j = 1;
+	while (j < ac)
+	{
+		ft_putstr(av[j]);
+		ft_putchar('\n');
+		j++;
+	}
+	return (0);
+}
